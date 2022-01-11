bc1863877b03ff468d969ddc9a797cec94139c68
Nahom Fikadu <nahi.terefe@gmail.com>
2022-01-11 05:10:20 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -45,7 +45,8 @@ void heap_sort(int *array, size_t size);
 
 void radix_sort(int *array, size_t size);
 
-void quick_sort_hoare(int *array, size_t size)
+void quick_sort_hoare(int *array, size_t size);
 
+void bitonic_sort(int *array, size_t size);
 
 #endif /* __sort__ */
